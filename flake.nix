c91d3a71db13ccbdf6ca64c8b1cbd159acfec1f9
theo <theodorusib@gmail.com>
2026-05-23 22:01:43 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Development shell with Node.js 24 and pnpm";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_24
+              pnpm
+            ];
+          };
+        });
+    };
+}
