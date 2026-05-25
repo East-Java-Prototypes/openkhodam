d5e85dce756a1cc26594e3c26de14b4af1f023d5
khodam666 <theobudiyanto@gmail.com>
2026-05-25 04:16:53 +0000

    Add TanStack routing and query providers

---
+++ packages/desktop/src/renderer/src/routes/__root.tsx
@@ -0,0 +1,8 @@
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import type { JSX } from 'react'
+
+export const Route = createRootRoute({ component: RootRoute })
+
+function RootRoute(): JSX.Element {
+  return <Outlet />
+}
