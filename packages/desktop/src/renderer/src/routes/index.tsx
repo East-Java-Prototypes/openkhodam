ab7957735b69646ecfbd93bd6694c3b4168c6705
khodam666 <theobudiyanto@gmail.com>
2026-05-25 04:22:46 +0000

    Render index route content

--- packages/desktop/src/renderer/src/routes/index.tsx
+++ packages/desktop/src/renderer/src/routes/index.tsx
@@ -1,8 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
 import type { JSX } from 'react'
 
 export const Route = createFileRoute('/')({ component: IndexRoute })
 
 function IndexRoute(): JSX.Element {
-  return <main />
+  return (
+    <main>
+      <h1>Index</h1>
+      <p>OpenKhodam desktop is running.</p>
+      <Link to="/settings">Open settings</Link>
+    </main>
+  )
 }
