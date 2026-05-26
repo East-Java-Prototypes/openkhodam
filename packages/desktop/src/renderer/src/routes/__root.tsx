39cb5bffd3a91678518a0f9f8322e8f5718f0d95
khodam666 <theobudiyanto@gmail.com>
2026-05-26 02:43:55 +0000

    Extract navbar component

--- packages/desktop/src/renderer/src/routes/__root.tsx
+++ packages/desktop/src/renderer/src/routes/__root.tsx
@@ -1,4 +1,5 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import AppNavbar from '../components/AppNavbar'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import type { JSX } from 'react'
 
 export const Route = createRootRoute({ component: RootRoute })
@@ -6,14 +7,7 @@ export const Route = createRootRoute({ component: RootRoute })
 function RootRoute(): JSX.Element {
   return (
     <div className="min-h-screen">
-      <header className="flex items-center justify-between px-4 py-3">
-        <Link className="rounded px-2 py-1" to="/">
-          Home
-        </Link>
-        <Link className="rounded px-2 py-1" to="/settings">
-          Settings
-        </Link>
-      </header>
+      <AppNavbar />
       <Outlet />
     </div>
   )
