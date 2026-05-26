5d843518961b2fba1c549e32753e06e4821966a5
khodam666 <theobudiyanto@gmail.com>
2026-05-26 02:43:55 +0000

    Extract navbar component

---
+++ packages/desktop/src/renderer/src/components/AppNavbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from '@tanstack/react-router'
+import type { JSX } from 'react'
+
+function AppNavbar(): JSX.Element {
+  return (
+    <header className="flex items-center justify-between px-4 py-3">
+      <Link className="rounded px-2 py-1" to="/">
+        Home
+      </Link>
+      <Link className="rounded px-2 py-1" to="/settings">
+        Settings
+      </Link>
+    </header>
+  )
+}
+
+export default AppNavbar
