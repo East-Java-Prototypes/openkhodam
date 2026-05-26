c103764e4b59ab4d649e61c0d732151429effd86
khodam666 <theobudiyanto@gmail.com>
2026-05-26 02:06:56 +0000

    Add barebones top navbar

--- packages/desktop/src/renderer/src/routes/__root.tsx
+++ packages/desktop/src/renderer/src/routes/__root.tsx
@@ -1,8 +1,20 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import type { JSX } from 'react'
 
 export const Route = createRootRoute({ component: RootRoute })
 
 function RootRoute(): JSX.Element {
-  return <Outlet />
+  return (
+    <div className="min-h-screen">
+      <header className="flex items-center justify-between px-4 py-3">
+        <Link className="rounded px-2 py-1" to="/">
+          Home
+        </Link>
+        <Link className="rounded px-2 py-1" to="/settings">
+          Settings
+        </Link>
+      </header>
+      <Outlet />
+    </div>
+  )
 }
