459c56b9955d06478fefa0ef21258e9b3d4b07ea
khodam666 <theobudiyanto@gmail.com>
2026-07-03 04:30:14 +0000

    Load linked docs in browser preview

--- packages/desktop/src/renderer/src/env.d.ts
+++ packages/desktop/src/renderer/src/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+import type { WebviewTag } from 'electron'
+import type { HTMLAttributes } from 'react'
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      webview: HTMLAttributes<WebviewTag> & {
+        src?: string
+      }
+    }
+  }
+}
