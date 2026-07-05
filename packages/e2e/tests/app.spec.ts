f74763774f4eb6ba5c916b4029549d65e38c94b5
khodam666 <theobudiyanto@gmail.com>
2026-07-05 12:06:44 +0000

    Update Google Workspace scope e2e expectation

--- packages/e2e/tests/app.spec.ts
+++ packages/e2e/tests/app.spec.ts
@@ -13,6 +13,8 @@ const googleWorkspaceNotConfiguredMessage =
   'Google OAuth client ID or client secret is not configured.'
 const emptyOpenedProjectsMessage = 'No opened project folders yet.'
 const googleDriveMetadataReadonlyScope = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+const googleSheetsSpreadsheetsReadonlyScope =
+  'https://www.googleapis.com/auth/spreadsheets.readonly'
 const fixtureLinkedDocUrl = 'https://docs.google.com/document/d/fixture-linked-doc/edit'
 const arbitraryLinkedDocUrl = 'https://example.test/document/d/arbitrary-linked-doc/edit'
 const hiddenSubagentSessionTitle = 'Hidden subagent child chat'
@@ -580,7 +582,14 @@ async function installGoogleWorkspaceOAuthCapture(electronApp: ElectronApplicati
         return originalFetch(input, init)
       }
     },
-    ['openid', 'email', 'profile', googleDriveMetadataReadonlyScope, googleDocsDocumentsScope]
+    [
+      'openid',
+      'email',
+      'profile',
+      googleDriveMetadataReadonlyScope,
+      googleDocsDocumentsScope,
+      googleSheetsSpreadsheetsReadonlyScope
+    ]
   )
 }
 
@@ -1949,6 +1958,7 @@ test.describe('Google Workspace connect cancellation', () => {
         'email',
         googleDocsDocumentsScope,
         googleDriveMetadataReadonlyScope,
+        googleSheetsSpreadsheetsReadonlyScope,
         'openid',
         'profile'
       ].sort()
@@ -1991,6 +2001,7 @@ test.describe('Google Workspace connect cancellation', () => {
         'email',
         googleDocsDocumentsScope,
         googleDriveMetadataReadonlyScope,
+        googleSheetsSpreadsheetsReadonlyScope,
         'openid',
         'profile'
       ],
